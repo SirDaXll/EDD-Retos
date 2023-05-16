9c01bab9312705a177d3f51ad6db3b8d9dd7fde7
El Diego <SirDaXll@users.noreply.github.com>
2023-05-16 10:38:53 -0400

    Update Lab2.2.dart

--- Lab2/Lab2.2.dart
+++ Lab2/Lab2.2.dart
@@ -2,5 +2,10 @@ import 'dart:math';
 
 void main(){
   List<int> Lista1 = [3, 4, 7, 9, 8, 5, 1, 2, 5, 7];
-  
+
+  List<int> ListaRandom = [];
+  for (int i = 0; i < 10; i++){
+    int aux = -(Random().nextInt(5) + 1);
+    ListaRandom.add(aux);
+  }
 }
