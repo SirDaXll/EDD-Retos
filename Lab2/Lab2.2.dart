e91255fc114b7fbee13f2efdf7fc4a5122f6e9f5
El Diego <SirDaXll@users.noreply.github.com>
2023-05-22 21:49:32 -0400

    Update Lab2.2.dart

--- Lab2/Lab2.2.dart
+++ Lab2/Lab2.2.dart
@@ -1,12 +1,27 @@
 import 'dart:math';
 
 void main(){
-  List<int> Lista1 = [3, 4, 7, 9, 8, 5, 1, 2, 5, 7];
+  List<int> Lista = [3, 4, 7, 9, 8, 5, 1, 2, 5, 7];
 
   List<int> ListaRandom = [];
   for (int i = 0; i < 10; i++){
     int aux = -(Random().nextInt(5) + 1);
     ListaRandom.add(aux);
   }
+
+  List<int> Resultante = [];
+
+  for (int i = 0; i < Lista.length; i++) {
+    int suma = Lista[i] + ListaRandom[i];
+    Resultante.add(suma);
+  }
+
+  print('Lista resultante de la suma de las listas:');
+  print(Resultante);
   
+  Resultante.removeAt(6); // Eliminar el séptimo elemento
+  Resultante.removeAt(6); // Eliminar el octavo elemento
+  
+  print('Lista resultante después de eliminar los elementos:');
+  print(Resultante);
 }
